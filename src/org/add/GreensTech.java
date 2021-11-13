2e55fc39495cdac3398f41a7ffbabe6219c7b89c
anushaml7 <anulax.rs@gmail.com>
2021-11-13 16:48:00 +0530

    Created by Anu

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,41 @@
+package org.add;
+
+public class GreensTech {
+	public static void main(String[] args) {
+		GreensTech add=new GreensTech();
+		add.greensOmr();
+		add.greensAdyar();
+		add.greensTambaram();
+		add.greensVelachery();
+		add.greensAnnanagar();
+	}
+	
+	public void greensOmr() {
+		System.out.println("---OMR---\r\n" + 
+				"No.19, Balamurugan Garden, OMR Road, Thoraipakkam,\r\n" + 
+				"Kancheepuram (DT).");
+	}
+	public void greensAdyar() {
+		System.out.println("---Adyar---\r\n" + 
+				"No.11 , First Street ,\r\n" + 
+				"Padmanabha Nagar , Adyar ,\r\n" + 
+				"Chennai-600 020.");
+	}
+	public void greensTambaram() {
+		System.out.println("---Tambaram---\r\n" + 
+				"No.1, Appa Rao colony,\r\n" + 
+				"Tambaram,\r\n" + 
+				"Sanatorium,\r\n" + 
+				"Chennai - 600 047.");
+	}
+	public void greensVelachery() {
+		System.out.println("---Velachery---\r\n" + 
+				"No.28, Nagendra Nagar, Opposite Phoenix Mall, Velachery, Chennai - 600 042.");
+	}
+	public void greensAnnanagar() {
+		System.out.println("---Anna Nagar---\r\n" + 
+				"SDV Arcade\r\n" + 
+				"4th floor, AB-5, 2nd Ave, Anna Nagar, Chennai - 600 040.");
+	}
+	
+}
